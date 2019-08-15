7d03bf40d1a8764df1580bdd04c0ec8a9e0bdcee
Andrey Larin <lestex@gmail.com>
2019-08-15 19:45:52 +0300

    adds rubocop and fixes to rb files

--- test/integration/simple_vpc/controls/gcloud.rb
+++ test/integration/simple_vpc/controls/gcloud.rb
@@ -1,26 +1,24 @@
 project_id   = attribute('project_id')
 network_name = attribute('network_name')
 
-control "gcloud" do
-  title "gcloud configuration"
+control 'gcloud' do
+  title 'gcloud configuration'
 
   describe command("gcloud compute networks subnets describe #{network_name}-subnet-01 --project=#{project_id} --region=us-west1 --format=json") do
     its(:exit_status) { should eq 0 }
     its(:stderr) { should eq '' }
 
     let(:data) do
-      if subject.exit_status == 0
+      if subject.exit_status.zero?
         JSON.parse(subject.stdout)
       else
         {}
       end
     end
 
-    describe "enableFlowLogs" do
-      it "should be false" do
-        expect(data).to include(
-          "enableFlowLogs" => false
-        )
+    describe 'enableFlowLogs' do
+      it 'should be false' do
+        expect(data).to include('enableFlowLogs' => false)
       end
     end
   end
@@ -30,18 +28,16 @@ control "gcloud" do
     its(:stderr) { should eq '' }
 
     let(:data) do
-      if subject.exit_status == 0
+      if subject.exit_status.zero?
         JSON.parse(subject.stdout)
       else
         {}
       end
     end
 
-    describe "enableFlowLogs" do
-      it "should be true" do
-        expect(data).to include(
-          "enableFlowLogs" => true
-        )
+    describe 'enableFlowLogs' do
+      it 'should be true' do
+        expect(data).to include('enableFlowLogs' => true)
       end
     end
   end
--- test/integration/simple_vpc/controls/gcp.rb
+++ test/integration/simple_vpc/controls/gcp.rb
@@ -1,8 +1,8 @@
 project_id   = attribute('project_id')
 network_name = attribute('network_name')
 
-control "gcp" do
-  title "Google Cloud configuration"
+control 'gcp' do
+  title 'Google Cloud configuration'
 
   describe google_compute_network(
     project: project_id,
@@ -14,20 +14,20 @@ control "gcp" do
   describe google_compute_subnetwork(
     project: project_id,
     name: "#{network_name}-subnet-01",
-    region: "us-west1"
+    region: 'us-west1'
   ) do
     it { should exist }
-    its('ip_cidr_range') { should eq "10.10.10.0/24" }
+    its('ip_cidr_range') { should eq '10.10.10.0/24' }
     its('private_ip_google_access') { should be false }
   end
 
   describe google_compute_subnetwork(
     project: project_id,
     name: "#{network_name}-subnet-02",
-    region: "us-west1"
+    region: 'us-west1'
   ) do
     it { should exist }
-    its('ip_cidr_range') { should eq "10.10.20.0/24" }
+    its('ip_cidr_range') { should eq '10.10.20.0/24' }
     its('private_ip_google_access') { should be true }
   end
 end
